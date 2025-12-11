42d98016645fd37caea0d21acf8203267359512c
josephmaxwellgekhf685 <lips.ebbs0d@icloud.com>
2025-12-11 15:49:33 +0800

    Add ESLint config, workflow, and example file with linting errors

---
+++ src/example.js
@@ -0,0 +1,7 @@
+// This file has intentional linting violations
+const unusedVariable = 42;
+console.log("This uses double quotes (violation)");
+function testFunction() {
+  let value = 10
+  return value
+}
